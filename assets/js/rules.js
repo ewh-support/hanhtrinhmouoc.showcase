d202a10b241091d2959d88604ca44a84c6ada934
showcase <showcase@email.com>
2018-05-14 10:52:14 +0700

    05/14/2018 10:52:10.34

--- assets/js/rules.js
+++ assets/js/rules.js
@@ -1,14 +1,45 @@
 $(document).ready(function () {
-    if ($('div').attr('rules-body') != undefined)
-        var rules_top = $('.rules-body').offset().top - 200;
+    var rules_top = $('.rules-body').offset().top - 100;
+    var running = false;
 
     $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
+        // selectors
+        var $window = $(window),
+            $body = $('body'),
+            $panel = $('.section');
+
+        var scroll = $window.scrollTop();
         if (scroll >= rules_top) {
             $('.rules-content').addClass('active');
-        }
-        else {
+        } else {
             $('.rules-content').removeClass('active');
         }
-    });
+
+        if (scroll > 200) {
+            $('.section__img__inner').css({
+                'top': '0'
+            });
+        } else {
+            $('.section__img__inner').css({
+                'top': '2000px'
+            });
+        }
+
+        // Change 22% earlier than scroll position so colour is there when you arrive.
+
+        scroll = $window.scrollTop() + ($window.height() / 2);
+
+        $('.section').removeClass('active');
+        $panel.each(function () {
+            var $this = $(this);
+
+            // if position is within range of this panel.
+            // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+            // Remember we set the scroll to 33% earlier in scroll var.
+            if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+                $this.addClass('active');
+            }
+        });
+
+    }).scroll();
 });
