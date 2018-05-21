20c766375c2e88ba28f277b0e63e096477737f89
showcase <showcase@email.com>
2018-05-21 16:27:51 +0700

    05/21/2018 16:27:47.62

--- assets/js/anh-du-thi.js
+++ assets/js/anh-du-thi.js
@@ -8,7 +8,7 @@ function loadXMLDoc(dname) {
     xhttp.send();
     return xhttp.responseXML;
 }
-var xmlDoc1 = loadXMLDoc("http://hanhtrinhmouoc.thanhnien.vn/tinrss?type=1"); // XML
+var xmlDoc1 = loadXMLDoc("http://hanhtrinhmouoc.thanhnien.vn/tinrss?type=2"); // XML
 var x2js = new X2JS();
 var jsonObj1 = x2js.xml2json(xmlDoc1); // Convert XML to JSON
 console.log(jsonObj1.rss.channel.item);
@@ -22,6 +22,6 @@ rivets.formatters.date = function (value) {
     return moment(value).format('DD.MM.YYYY')
 }
 
-rivets.bind($('#bind-bai-viet'), {
+rivets.bind($('#bind-bai-anh'), {
     items: jsonObj1.rss.channel.item
 })
