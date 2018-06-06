d22d5babc34d15e1196e6710e57e843ccde5fee7
showcase <showcase@email.com>
2018-06-06 10:40:06 +0700

    06/06/2018 10:40:02.45

--- assets/js/anh-du-thi.js
+++ assets/js/anh-du-thi.js
@@ -1,3 +1,4 @@
+console.log('object');
 function loadXMLDoc(dname) {
     if (window.XMLHttpRequest) {
         xhttp = new XMLHttpRequest();
@@ -11,7 +12,7 @@ function loadXMLDoc(dname) {
 var xmlDoc1 = loadXMLDoc("http://hanhtrinhmouoc.thanhnien.vn/tinrss?type=2"); // XML
 var x2js = new X2JS();
 var jsonObj1 = x2js.xml2json(xmlDoc1); // Convert XML to JSON
-// console.log(jsonObj1.rss.channel.item);
+console.log(jsonObj1.rss.channel.item);
 
 /* Binding */
 rivets.binders.src = function (el, value) {
