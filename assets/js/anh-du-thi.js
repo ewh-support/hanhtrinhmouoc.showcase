169afdcec7b23f5b28f6f58b2597fd9e813af95e
showcase <showcase@email.com>
2018-06-06 16:53:49 +0700

    06/06/2018 16:53:41.49

--- assets/js/anh-du-thi.js
+++ assets/js/anh-du-thi.js
@@ -1,4 +1,3 @@
-console.log('object');
 function loadXMLDoc(dname) {
     if (window.XMLHttpRequest) {
         xhttp = new XMLHttpRequest();
@@ -12,7 +11,7 @@ function loadXMLDoc(dname) {
 var xmlDoc1 = loadXMLDoc("http://hanhtrinhmouoc.thanhnien.vn/tinrss?type=2"); // XML
 var x2js = new X2JS();
 var jsonObj1 = x2js.xml2json(xmlDoc1); // Convert XML to JSON
-console.log(jsonObj1.rss.channel.item);
+//console.log(jsonObj1.rss.channel.item);
 
 /* Binding */
 rivets.binders.src = function (el, value) {
@@ -27,12 +26,41 @@ rivets.formatters.date = function (value) {
     return moment(value).format('DD.MM.YYYY')
 }
 
-rivets.bind($('#bind-bai-anh-moi-nhat'), {
+var row1Col2 = [];
+var row2Col1 = [];
+var row3Col2 = [];
+
+for (let i = 1; i <= 2; i++) {
+    row1Col2.push(jsonObj1.rss.channel.item[i]);
+}
+console.log(row1Col2);
+for (let i = 4; i <= 5; i++) {
+    row2Col1.push(jsonObj1.rss.channel.item[i]);
+}
+
+for (let i = 7; i <= 8; i++) {
+    row3Col2.push(jsonObj1.rss.channel.item[i]);
+}
+
+rivets.bind($('#bind-row1-col1'), {
     items: jsonObj1.rss.channel.item[0]
 })
 
-jsonObj1.rss.channel.item.shift();
+rivets.bind($('#bind-row1-col2'), {
+    items: row1Col2
+})
 
-rivets.bind($('#bind-bai-anh'), {
-    items: jsonObj1.rss.channel.item
+rivets.bind($('#bind-row2-col1'), {
+    items: row2Col1
+})
+
+rivets.bind($('#bind-row2-col2'), {
+    items: jsonObj1.rss.channel.item[3]
+})
+rivets.bind($('#bind-row3-col1'), {
+    items: jsonObj1.rss.channel.item[6]
+})
+
+rivets.bind($('#bind-row3-col2'), {
+    items: row3Col2
 })
---
+++ assets/js/index.js
@@ -0,0 +1,84 @@
+console.log('object');
+
+function loadXMLDoc(dname) {
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    } else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET", dname, false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+var x2js = new X2JS();
+
+
+var xmlDoc1 = loadXMLDoc("http://hanhtrinhmouoc.thanhnien.vn/tinrss?type=2"); // XML
+var jsonObj1 = x2js.xml2json(xmlDoc1); // Convert XML to JSON
+console.log(jsonObj1.rss.channel.item);
+
+/* Binding */
+rivets.binders.src = function (el, value) {
+    el.src = value;
+};
+
+rivets.binders.href = function (el, value) {
+    el.href = value;
+}
+
+rivets.formatters.date = function (value) {
+    return moment(value).format('DD.MM.YYYY')
+}
+
+rivets.bind($('#bind-bai-anh-moi-nhat'), {
+    items: jsonObj1.rss.channel.item[0]
+})
+
+rivets.bind($('#bind-new-second'), {
+    items: jsonObj1.rss.channel.item[1]
+})
+
+var col1Row2 = [];
+for (let i = 3; i <= 4; i++) {
+    col1Row2.push(jsonObj1.rss.channel.item[i]);
+}
+
+var col2Row1 = [];
+for (let i = 5; i <= 6; i++) {
+    col2Row1.push(jsonObj1.rss.channel.item[i]);
+}
+
+rivets.bind($('#bind-col1-row2'), {
+    items: col1Row2
+})
+
+rivets.bind($('#bind-col2-row1'), {
+    items: col2Row1
+})
+
+//bài dự thi
+var xmlDoc2 = loadXMLDoc("http://hanhtrinhmouoc.thanhnien.vn/tinrss?type=1"); // XML
+var jsonObj2 = x2js.xml2json(xmlDoc2); // Convert XML to JSON
+console.log('obj2', jsonObj2.rss.channel.item);
+
+var newest_blog = [];
+for (let i = 0; i < 6; i++) {
+    newest_blog.push(jsonObj2.rss.channel.item[i]);
+}
+
+rivets.bind($('#bind-bai-viet'), {
+    items: newest_blog
+})
+
+rivets.binders.src = function (el, value) {
+    el.src = value;
+};
+
+rivets.binders.href = function (el, value) {
+    el.href = '/bai-chi-tiet/?id=' + value
+}
+
+rivets.formatters.date = function (value) {
+    return moment(value).format('DD.MM.YYYY')
+}
+ 
